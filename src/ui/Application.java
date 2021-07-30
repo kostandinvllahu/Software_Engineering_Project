20b9c788e31a53d0c4d342984771593bb2f72cda
kostandinvllahu <Ghostsechackers@gmail.com>
2021-07-30 13:31:40 +0200

    Fixed the bug where some images would show and some not

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -7,10 +7,9 @@ import org.openstreetmap.gui.jmapviewer.interfaces.ICoordinate;
 import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
 import org.openstreetmap.gui.jmapviewer.tilesources.BingAerialTileSource;
 import query.Query;
-import twitter.PlaybackTwitterSource;
+import twitter.LiveTwitterSource;
 import twitter.TwitterSource;
 import util.SphericalGeometry;
-import util.Util;
 
 import javax.swing.*;
 import java.awt.*;
@@ -23,7 +22,6 @@ import java.util.Timer;
 /**
  * The Twitter viewer application
  * Derived from a JMapViewer demo program written by Jan Peter Stotz
- * NDRYSHIME KODI TE RRJESHTI 126-130 DHE 59 , 199-203
  */
 public class Application extends JFrame {
     // The content panel, which contains the entire UI
@@ -35,43 +33,11 @@ public class Application extends JFrame {
     // The source of tweets, a TwitterSource, either live or playback
     private TwitterSource twitterSource;
 
-    private void initialize() {
-        // To use the live twitter stream, use the following line
-        // twitterSource = new LiveTwitterSource();
-
-        // To use the recorded twitter stream, use the following line
-        // The number passed to the constructor is a speedup value:
-        //  1.0 - play back at the recorded speed
-        //  2.0 - play back twice as fast
-        twitterSource = new PlaybackTwitterSource(60.0);
-
-        queries = new ArrayList<>();
-    }
-
     /**
-     * A new query has been entered via the User Interface
-     * @param   query   The new query object
+     * @param args Application program arguments (which are ignored)
      */
-    public void addQuery(Query query) {
-    	 queries.add(query);
-         Set<String> allterms = getQueryTerms();
-         twitterSource.setFilterTerms(allterms);
-         contentPanel.addQuery(query);
-         twitterSource.addObserver(query);
-        // TODO: This is the place where you should connect the new query to the twitter source
-    }
-
-    /**
-     * return a list of all terms mentioned in all queries. The live twitter source uses this
-     * to request matching tweets from the Twitter API.
-     * @return
-     */
-    private Set<String> getQueryTerms() {
-        Set<String> ans = new HashSet<>();
-        for (Query q : queries) {
-            ans.addAll(q.getFilter().terms());
-        }
-        return ans;
+    public static void main(String[] args) {
+        new Application().setVisible(true);
     }
 
     /**
@@ -124,15 +90,51 @@ public class Application extends JFrame {
             public void mouseMoved(MouseEvent e) {
                 Point p = e.getPoint();
                 ICoordinate pos = map().getPosition(p);
-                // TODO: Use the following method to set the text that appears at the mouse cursor
                 List<MapMarker> mapMarkers = getMarkersCovering(pos, pixelWidth(p));
                 MapMarkerCircleOne marker = (MapMarkerCircleOne) mapMarkers.get(mapMarkers.size() - 1);
-                if(!mapMarkers.isEmpty()) {
-                	map().setToolTipText("<html>" + "<img src=" + marker.getUserProfileImageURL() + ">" + marker.getTweet() + "</html>");
+                if (!mapMarkers.isEmpty()) {
+                    map().setToolTipText("<html>" + "<img src=" + marker.getUserProfileImageURL() + ">" + marker.getTweet() + "</html>");
                 }
-               // map().setToolTipText("This is a tooltip");
             }
         });
+    }
+
+    private void initialize() {
+        // To use the live twitter stream, use the following line
+        twitterSource = new LiveTwitterSource();
+
+        // To use the recorded twitter stream, use the following line
+        // The number passed to the constructor is a speedup value:
+        //  1.0 - play back at the recorded speed
+        //  2.0 - play back twice as fast
+        // twitterSource = new PlaybackTwitterSource(60.0);
+
+        queries = new ArrayList<>();
+    }
+
+    /**
+     * A new query has been entered via the User Interface
+     * @param   query   The new query object
+     */
+    public void addQuery(Query query) {
+        queries.add(query);
+        Set<String> allterms = getQueryTerms();
+        twitterSource.setFilterTerms(allterms);
+        contentPanel.addQuery(query);
+        twitterSource.addObserver(query);
+    }
+
+    /**
+     * return a list of all terms mentioned in all queries. The live twitter source uses this
+     * to request matching tweets from the Twitter API.
+     * @return
+     */
+    private Set<String> getQueryTerms() {
+        Set<String> ans = new HashSet<>();
+        for (Query q : queries) {
+            ans.addAll(q.getFilter().terms());
+        }
+        return ans;
     }
 
     // How big is a single pixel on the map?  We use this to compute which tweet markers
@@ -172,13 +174,6 @@ public class Application extends JFrame {
         return contentPanel.getViewer();
     }
 
-    /**
-     * @param args Application program arguments (which are ignored)
-     */
-    public static void main(String[] args) {
-        new Application().setVisible(true);
-    }
-
     // Update which queries are visible after any checkBox has been changed
     public void updateVisibility() {
         SwingUtilities.invokeLater(new Runnable() {
@@ -196,8 +191,7 @@ public class Application extends JFrame {
 
     // A query has been deleted, remove all traces of it
     public void terminateQuery(Query query) {
-        // TODO: This is the place where you should disconnect the expiring query from the twitter source
-    	twitterSource.deleteObserver(query);
+        twitterSource.deleteObserver(query);
         queries.remove(query);
         Set<String> allterms = getQueryTerms();
         twitterSource.setFilterTerms(allterms);
